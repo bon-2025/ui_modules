2011cbda8794f3f29bdef69df537f3facc48fe34
bon-2025 <capstone27062025@email.com>
2025-12-07 16:47:21 +0800

    collaborate

--- registers-module/src/services/registrationService.js
+++ registers-module/src/services/registrationService.js
@@ -1,8 +1,17 @@
-export const registerUser = async (data) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log("User registered:", data);
-      resolve({ success: true });
-    }, 1000);
-  });
-};
+
+export async function registerUser(data) {
+  try {
+    const response = await fetch("http://localhost:5000/deaths", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+
+    if (!response.ok) throw new Error("Failed to save data.");
+
+    const result = await response.json();
+    return { success: true, data: result };
+  } catch (error) {
+    return { success: false, message: error.message };
+  }
+}
